bd3a16e27640b0909067200116904ed12f9229f6
Animesh Singh <animeshsingh.mail@gmail.com>
2023-06-14 22:21:02 +0530

    styled and made layout for header

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,10 +1,25 @@
 const ListHeader = ({listName})=> {
+const signOut = () =>
+{
+  console.log("temp signout");
+
+}
+const addNew = () =>
+{
+  console.log("temp addnew");
+
+}
+
     return (
       <div className="ListHeader">
       <h1>{listName}</h1>
+      <div className="button-container">
+      <button className="create" onClick={addNew}>ADD NEW</button>
+      <button className="signout" onClick={signOut}>SIGN OUT</button>
+      </div>
       </div>
     );
   }
   
   export default ListHeader;
-  
+   
